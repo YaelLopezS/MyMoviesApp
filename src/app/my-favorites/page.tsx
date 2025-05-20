30b36f456475021fe0a5fa7d524fc623f52ad501
YaelLopezS <149212804+YaelLopezS@users.noreply.github.com>
2025-05-19 23:03:55 -0600

    Update page.tsx

--- src/app/my-favorites/page.tsx
+++ src/app/my-favorites/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import { useEffect, useState } from "react";
 import { useGuestSession } from "@/providers/GuestSessionContext";
 import { getFavoriteMovies } from "@/services/accounts/getFavoriteMovies";
